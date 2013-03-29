337a0508ae1966f9891c8e8a222f10a877ccf401
Mariano <macodarini@zappos.com>
2013-03-29 15:16:29 -0700

    final b3

--- src/main/java/bang/bang/App.java
+++ src/main/java/bang/bang/App.java
@@ -217,7 +217,7 @@ public class App
 				}
 				//read file for command to play the game
 				action = line.split(",");
-				System.out.println(action[0] + action[1] + action[2]);
+				System.out.println(action[0] + "," + action[1] + "," + action[2]);
 
 				if(action.length < 1){
 					// something is wrong
@@ -227,7 +227,7 @@ public class App
 					 * to do
 					 * add cards to correct hand
 					 */
-					System.out.println("b3 command");
+					//System.out.println("b3 command");
 					b3Button(action[2]);
 					
 					
@@ -472,9 +472,6 @@ public class App
 			}
 		}
 	}
-		
-
-	
 
 	private static void takeCardFromHand(String cardType,String card) {
 		int idxOfHand = 0;
@@ -547,81 +544,9 @@ public class App
 		int playerIndex;
 		int sheriffPos = findSheriff();
 		bangs = 0;
-		
-//		if (card.equals("jail"))
-//			play("miss turn");
-//		if (card.equals("dynamite")) {
-//			health = health - 3;
-//			if (health <=0) {
-//				for (int i = 0; i < bHand.size(); i++) {
-//					currentCard = hand.get(i);
-//					if (currentCard.equals("beer")) {
-//						play("Hand: " + i + " " +  printArray[17]);
-//						break;
-//					}
-//				}	
-//			}
-//			else
-//				play(printArray[56]);
-//		}
-		System.out.println("in b3!");
+
 		
 		playBlueHand();
-//		for (int i = 0; i < bHand.size(); i++) {
-//			System.out.println("looping blue hand");
-//			currentCard = bHand.get(i);
-//			
-//			if (currentCard.equals("jail")) {
-//				do
-//					playerIndex = choosePlayerIndex(roles.size());
-//				while (playerIndex != sheriffPos);
-//			}
-//			if (currentCard.equals("dynamite")) {
-//				playerIndex = choosePlayerIndex(roles.size());
-//			}
-//			if (currentCard.equals("binocular")) {
-//				rangeOther++;
-//				bHand.remove(i);
-//				play("Hand: " + i +  printArray[54]);
-//			}
-//			if (currentCard.equals("scope")) {
-//				rangeOther++;
-//				bHand.remove(i);
-//				play("Hand: " + i +  printArray[53]);
-//			}
-//			if (currentCard.equals("barrel") || currentCard.equals("hideout") || currentCard.equals("mustang")) {
-//				bHand.remove(i);
-//				play("Hand: " + i);
-//			}
-//			if (currentCard.equals("schofield") && rangeGun < 2) {
-//				rangeGun = 2;
-//				bHand.remove(i);
-//				play("Hand: " + i +  printArray[48]);
-//			}
-//			if (currentCard.equals("remindton") && rangeGun < 3){
-//				rangeGun = 3;
-//				bHand.remove(i);
-//				play("Hand: " + i +  printArray[49]);
-//			}
-//			if (currentCard.equals("schofield") && rangeGun < 4){
-//				rangeGun = 4;
-//				bHand.remove(i);
-//				play("Hand: " + i +  printArray[48]);
-//			}
-//			if (currentCard.equals("schofield") && rangeGun < 5){
-//				rangeGun = 5;
-//				bHand.remove(i);
-//				play("Hand: " + i +  printArray[49]);
-//			}
-//			if (currentCard.equals("volcanic")) {
-//				if (!(myRole.contains("outlaw") && sheriffPos > 1)) {
-//					rangeGun = 1;
-//					isVolcanic = true;
-//					bHand.remove(i);
-//					play("Hand: " + i +  printArray[66]);
-//				}
-//			}
-//		}
 		
 		for (int i = 0; i < gHand.size(); i++) {
 			GreenHand currentGreen = gHand.get(i);
@@ -633,34 +558,7 @@ public class App
 		}
 		
 		playGreenHand();
-//		for (int i = 0; i < gHand.size(); i++) {
-//			System.out.println("looping green hand");
-//
-//			GreenHand currentGreen = gHand.get(i);	
-//			String cGCard = currentGreen.getCard();
-//			
-//			if (currentGreen.isActive()) {	
-//				if (cGCard.equals("contestoga") || cGCard.equals("cancan") || cGCard.equals("pepperbox")
-//						|| cGCard.equals("howitzer") || cGCard.equals("buffalorifle") || cGCard.equals("knife")
-//						|| cGCard.equals("derringer")) {
-//					play(String.valueOf("Green Hand: " + i + " On player: " + choosePlayerIndex(myRange)) +  printArray[25]);
-//				}
-//				if (cGCard.equals("canteen") && health < 4) {
-//					gHand.remove(i);
-//					play("Green Hand: " + i +  printArray[19]);
-//				}
-//				if (cGCard.equals("ponyexpress")) {
-//					gHand.remove(i);
-//					play("Green Hand: " + i +  printArray[14]);
-//				}
-//			}
-//			
-//			if (!currentGreen.isInPlay()) {
-//				currentGreen.play();
-//				gHand.set(i,currentGreen);
-//				play("green card" + i +  printArray[10]);
-//			}
-//		}
+
 		for (int i = 0; i < gHand.size(); i++) {
 			GreenHand currentGreen = gHand.get(i);	
 //			String cGCard = currentGreen.getCard();
@@ -673,62 +571,6 @@ public class App
 		
 		playHand();
 		
-//		for (int i = 0; i < hand.size(); i++) {
-//			System.out.println("looping hand");
-//
-//			String cBCard = hand.get(i);
-//			
-//			if (cBCard.equals("panic")) {
-//				hand.remove(i);
-//				play(String.valueOf("Hand: " + i + " On player: " + choosePlayerIndex(rangeOther)) +  printArray[43]);
-//			}
-//			if (cBCard.equals("ragtime") || cBCard.equals("brawl")) {
-//				hand.remove(i);
-//				play(String.valueOf("Hand: " + i + " and " + randomCard(i) + " On player: " + choosePlayerIndex(rangeOther)) +  printArray[44]);
-//			}
-//			if (cBCard.equals("cat")) {
-//				hand.remove(i);
-//				play(String.valueOf("Hand: " + i + " On player: " + choosePlayerIndex(roles.size())) +  printArray[47]);
-//			}
-//			if (cBCard.equals("bang")) {
-//				hand.remove(i);
-//				play(String.valueOf("Hand: " + i + " On player: " + choosePlayerIndex(myRange)) +  printArray[28]);
-//			}
-//			if (cBCard.equals("gatling")) {
-//				hand.remove(i);
-//				play("Hand: " + i + " On player: " + choosePlayerIndex(myRange) +  printArray[29]);
-//			}
-//			if (cBCard.equals("punch")) {
-//				hand.remove(i);
-//				play(String.valueOf("Hand: " + i + " On player: " + choosePlayerIndex(rangeOther)) +  printArray[30]);
-//			}
-//			if (cBCard.equals("springfield") || cBCard.equals("duel")) {
-//				hand.remove(i);
-//				play(String.valueOf("Hand: " + i + " and " + randomCard(i) + " On player: " + choosePlayerIndex(roles.size())) +  printArray[8]);
-//			}
-//			if (cBCard.equals("indians") || cBCard.equals("wellsfargo") || cBCard.equals("stagecoach")) {
-//				hand.remove(i);
-//				play("Hand: " +  + i +  printArray[15]);
-//			}
-//			if (health < 4) {
-//				if (cBCard.equals("beer")) {
-//					hand.remove(i);
-//					play("Hand: " + i +  printArray[17]);
-//				}
-//				if (cBCard.equals("saloon")) {
-//					hand.remove(i);
-//					play("Hand: " + i +  printArray[18]);
-//				}
-//				if (cBCard.equals("whiskey")) {
-//					hand.remove(i);
-//					play(String.valueOf("Hand: " + i + " and " + randomCard(i)) +  printArray[21]);
-//				}
-//				if (cBCard.equals("tequila")) {
-//					hand.remove(i);
-//					play(String.valueOf("Hand: " + i + " and " + randomCard(i)) +  printArray[20]);
-//				}
-//			}
-//		}
 	}
 	
 	public static void playBlueHand() {
@@ -818,13 +660,18 @@ public class App
 	}
 	
 	public static void playGreenHand() {
-		System.out.println("looping green hand");
 		for (int i = 0; i < gHand.size(); i++) {	
 
 			GreenHand currentGreen = gHand.get(i);	
 			String cGCard = currentGreen.getCard();
 			
 			if (currentGreen.isActive()) {	
+				if (cGCard.equals("canteen") && (health < 4 || (myRole.equals("sheriff") && health < 5))) {
+					gHand.remove(i);
+					health++;
+					play("Green Hand: " + i + "--" + printArray[19]);
+					playGreenHand();
+				}
 				if (cGCard.equals("contestoga")) {
 					gHand.remove(i);
 					play(String.valueOf("Green Hand: " + i + " On player: " + choosePlayerIndex(myRange)) + "--" + printArray[42]);
@@ -860,11 +707,6 @@ public class App
 					play(String.valueOf("Green Hand: " + i + " On player: " + choosePlayerIndex(myRange)) + "--" + printArray[26]);
 					playGreenHand();
 				}
-				if (cGCard.equals("canteen") && health < 4) {
-					gHand.remove(i);
-					play("Green Hand: " + i + "--" + printArray[19]);
-					playGreenHand();
-				}
 				if (cGCard.equals("ponyexpress")) {
 					gHand.remove(i);
 					play("Green Hand: " + i + "--" + printArray[14]);
@@ -877,7 +719,6 @@ public class App
 	}
 	
 	public static void playHand() {
-		System.out.println("looping hand");
 		for (int i = 0; i < hand.size(); i++) {
 
 			String cBCard = hand.get(i);
@@ -1058,7 +899,7 @@ public class App
 			index++;
 		
 		if (myRole.equals("sheriff")) {
-			System.out.println(index);
+			//System.out.println(index);
 			return index;
 		}
 		else 
@@ -1075,11 +916,11 @@ public class App
 				index = sheriffPos;
 		}
 		if (direction == 1) {
-			System.out.println(index);
+			//System.out.println(index);
 			return Math.abs(roles.size() - index);
 		}
 		else {
-			System.out.println(index);
+			//System.out.println(index);
 			return index;
 		}
 	}
